2f26673eebfcd443e8e341d1f932779cdd010cf5
Paolo Brasolin <paolo.brasolin@gmail.com>
2023-07-24 01:18:45 +0200

    feat: pretty human readable output

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
-import Denomander from "https://deno.land/x/denomander/mod.ts";
+import Denomander, { Option } from "https://deno.land/x/denomander/mod.ts";
+import * as Colors from "https://deno.land/std@0.195.0/fmt/colors.ts";
+import cliFormatDeno from "https://deno.land/x/cli_format_deno@3.0.10/src/mod.ts";
+
 import { demauro } from "./lib/demauro.ts";
 import { treccani } from "./lib/treccani.ts";
 import { onli } from "./lib/onli.ts";
@@ -13,14 +16,39 @@ if (import.meta.main) {
 
   program
     .command("query [expression]", "Start a query")
+    .option("-p --poly", "Include polyrhematic results")
+    .addOption(
+      new Option({
+        flags: "-f --format",
+        description: "Select output format",
+      }).choices(["json", "human"]),
+    )
     .action(async ({ expression }: { expression: string }) => {
       const results = (await Promise.all([
         demauro(expression),
         treccani(expression),
         onli(expression),
       ])).flat();
-      console.log(JSON.stringify(results, null, 2));
+
+      switch (program.format) {
+        case "json":
+          console.log(JSON.stringify(results, null, 2));
+          break;
+        default:
+          for (const result of results) {
+            console.log(Colors.yellow("-".repeat(80) + "\n"));
+            console.log(Colors.dim(`SourceUrl: ${result.source.toString()}`));
+            console.log(Colors.dim(`Published: ${result.published || "N/D"}`));
+            const readable = result.fullText?.trim().replace(
+              /\b\d\d\d\d\b/g,
+              (year) => Colors.red(Colors.bold(year)),
+            );
+            console.log(
+              "\n" + cliFormatDeno.wrap(readable!, { width: 80 }) + "\n",
+            );
+          }
+          break;
+      }
     })
-    .option("-p --poly", "Include polyrhematic results")
     .parse(Deno.args);
 }
