325ee17c2c52e5e7d8062575029e11af9695f4eb
Paolo Brasolin <paolo.brasolin@gmail.com>
2023-07-23 16:47:47 +0200

    feat: cli interface

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
+import Denomander from "https://deno.land/x/denomander/mod.ts";
 import { demauro } from "./lib/demauro.ts";
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  const results = await demauro("pene");
-  console.log(JSON.stringify(results, null, 2));
+  const program = new Denomander({
+    app_name: "big-dict",
+    app_description: "Queries all the dictionaries with a single command",
+    app_version: "0.1.0",
+  });
+
+  program
+    .command("query [expression]", "Start a query")
+    .action(async ({ expression }: { expression: string }) => {
+      const results = await demauro(expression);
+      console.log(JSON.stringify(results, null, 2));
+    })
+    .option("-p --poly", "Include polyrhematic results")
+    .parse(Deno.args);
 }
